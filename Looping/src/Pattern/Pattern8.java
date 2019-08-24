b743e5cdd28410051e4a6b7bdd14e840f7be2dcd
Nikhil <Nikhil@DESKTOP-IKMS51V>
2019-08-24 19:13:31 +0530

    trying

--- Looping/src/Pattern/Pattern8.java
+++ Looping/src/Pattern/Pattern8.java
@@ -1,9 +1,12 @@
 package Pattern;
 
-public class Pattern4 {
+public class Pattern8 {
 	public static void main(String[] args) {
 		for(int i=1;i<=6;i++) {
-			for(int j=6;j>=i;j--) {
+			for(int j=1;j<=6-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
 				System.out.print("* ");
 			}
 			System.out.println();
